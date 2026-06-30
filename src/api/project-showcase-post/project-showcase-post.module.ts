6ed48fe9a2e095eac77dc7559659d678574d143d
Vasilica Olariu <olariu.vasilica@gmail.com>
2026-06-30 10:39:44 +0300

    PM-5307 - fix swagger for project showcase posts

--- src/api/project-showcase-post/project-showcase-post.module.ts
+++ src/api/project-showcase-post/project-showcase-post.module.ts
@@ -2,17 +2,23 @@ import { Module } from '@nestjs/common';
 import { GlobalProvidersModule } from 'src/shared/modules/global/globalProviders.module';
 import { ProjectShowcasePostController } from './project-showcase-post.controller';
 import { ProjectShowcasePostService } from './project-showcase-post.service';
-import { ProjectPostCategoryModule } from './project-post-category/project-post-category.module';
-import { ProjectPostIndustryModule } from './project-post-industry/project-post-industry.module';
+import { ProjectPostCategoryController } from './project-post-category/project-post-category.controller';
+import { ProjectPostIndustryController } from './project-post-industry/project-post-industry.controller';
+import { ProjectPostCategoryService } from './project-post-category/project-post-category.service';
+import { ProjectPostIndustryService } from './project-post-industry/project-post-industry.service';
 
 @Module({
-  imports: [
-    GlobalProvidersModule,
-    ProjectPostCategoryModule,
-    ProjectPostIndustryModule,
+  imports: [GlobalProvidersModule],
+  controllers: [
+    ProjectShowcasePostController,
+    ProjectPostCategoryController,
+    ProjectPostIndustryController,
   ],
-  controllers: [ProjectShowcasePostController],
-  providers: [ProjectShowcasePostService],
+  providers: [
+    ProjectShowcasePostService,
+    ProjectPostCategoryService,
+    ProjectPostIndustryService,
+  ],
   exports: [ProjectShowcasePostService],
 })
 export class ProjectShowcasePostModule {}
